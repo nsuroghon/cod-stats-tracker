a191d80f0c2b68687684886e90fec30b7b7deebf
Niv Suroghon <nsuroghon@gmail.com>
2021-05-27 13:11:24 -0400

    env variables & server functional

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+//load env
+dotenv.config({ path: './config.env'})
+
 const app = express();
 
-app.listen(5000, () => {
-    console.log(`app listening on port 5000`)
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} on port ${port}`)
 })
